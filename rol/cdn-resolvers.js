4b04887ed5ac18e993d8ec61dbaeeec2261afbd0
sultan99 <sultan99@mail.ru>
2019-04-09 15:31:58 +0600

    Added styled-components in cdn-resolvers.

--- rol/cdn-resolvers.js
+++ rol/cdn-resolvers.js
@@ -8,6 +8,12 @@ const resolve = (name, version) => {
       url: `https://unpkg.com/react-on-lambda@${version}/dist/react-on-lambda.min.js`,
       version
     },
+    'styled-components': {
+      name: `styled-components`,
+      var: `styled`,
+      url: `https://unpkg.com/styled-components@${version}/dist/styled-components.min.js`,
+      version
+    },
   }
   return list[name]
 }
